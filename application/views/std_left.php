397b6a1420ba84af5099fad6fae3c52cce4d6db4
johnnyeven_work <johnnyeven@gmail.com>
2013-05-30 11:07:25 +0800

    1

--- application/views/std_left.php
+++ application/views/std_left.php
@@ -24,22 +24,6 @@
             	<li class="last"><a href="<?php echo $root_path; ?>setting/permissions">权限设置</a></li>
             </ul>
         </li>
-        <li><a href="javascript:void(0)">网站内容管理</a>
-        	<ul<?php if(in_array($permission_name, array('web_webs', 'web_channel', 'web_article_list', 'web_article_add', 'web_media_list', 'web_media_video_add', 'web_media_pic_add', 'web_slide'))): ?> class="current"<?php endif; ?>>
-                <li><a href="<?php echo $root_path; ?>content/webs">网站列表</a></li>
-                <li><a href="<?php echo $root_path; ?>content/channels">频道管理</a></li>
-                <li><a href="<?php echo $root_path; ?>content/articles/lists">文章管理</a></li>
-                <li><a href="<?php echo $root_path; ?>content/articles/add">添加文章</a></li>
-                <li><a href="<?php echo $root_path; ?>content/media/lists">视频/图片管理</a></li>
-                <li class="last"><a href="javascript:void(0)">上传视频/图片</a>
-                	<ul<?php if(in_array($permission_name, array('web_media_video_add', 'web_media_pic_add', 'web_slide'))): ?> class="current"<?php endif; ?>>
-                    	<li><a href="<?php echo $root_path; ?>content/media/add_video">上传视频</a></li>
-                    	<li><a href="<?php echo $root_path; ?>content/media/add_pic">上传图片</a></li>
-						<li class="last"><a href="<?php echo $root_path; ?>content/slides">幻灯管理</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
         <!--
         <li><a href="javascript:void(0)">运维数据管理</a>
             <ul<?php if(in_array($permission_name, array('maintenance_product', 'maintenance_master', 'maintenance_chat'))): ?> class="current"<?php endif; ?>>
@@ -61,7 +45,7 @@
         	<ul<?php if(in_array($permission_name, array('report_overview', 'report_recharge_log', 'report_recharge_daily_statistics', 'report_recharge_account', 'report_account_buy', 'report_item_statistics',
 			'report_register_statistics', 'report_register_by_time', 'report_register_by_day', 'report_register_detail', 'report_login_detail', 'report_login_statistics', 'report_relogin_statistics', 
 			'report_active_account', 'report_remain_account', 'report_lost_account', 'report_daily_statistics'))): ?> class="current"<?php endif; ?>>
-                <li><a href="<?php echo $root_path; ?>report/daily_statistics">运营概况总览</a></li>
+                <li class="last"><a href="<?php echo $root_path; ?>report/daily_statistics">运营概况总览</a></li>
                 <!--
                 <li><a href="javascript:void(0)">充值情况</a>
                 	<ul<?php if(in_array($permission_name, array('report_recharge_log', 'report_recharge_daily_statistics', 'report_recharge_account', 'report_daily_statistics'))): ?> class="current"<?php endif; ?>>
@@ -98,7 +82,7 @@
         <li><a href="javascript:void(0)">GM管理</a>
         	<ul<?php if(in_array($permission_name, array('master_grant', 'master_ban'))): ?> class="current"<?php endif; ?>>
             	<li><a href="<?php echo $root_path; ?>master/grant">道具发放</a></li>
-            	<!--<li class="last"><a href="<?php echo $root_path; ?>master/ban">封停帐号</a></li>-->
+            	<li class="last"><a href="<?php echo $root_path; ?>master/ban">封停帐号</a></li>
             </ul>
         </li>
     </ul>
